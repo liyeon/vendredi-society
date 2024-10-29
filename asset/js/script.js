f18e852b7224a85142eefee06e095f8aa69542d2
liyeon <fldus612@gmail.com>
2024-10-29 17:57:01 +0900

    🚧 wip

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,44 +1,72 @@
-// 커서
-let mouseCursor = document.querySelector(".cursor");
-let links = document.querySelectorAll("a, button");
+$(document).ready(function () {
+  // 커서
+  let mouseCursor = document.querySelector(".cursor");
+  let links = document.querySelectorAll("a, button");
 
-// 목표 
-let targetX = 0, targetY = 0;
-// 현재
-let currentX = 0, currentY = 0;
-const followSpeed = 0.1; // 커서 속도
+  // 목표
+  let targetX = 0,
+    targetY = 0;
+  // 현재
+  let currentX = 0,
+    currentY = 0;
+  const followSpeed = 0.1; // 커서 속도
 
-// 커서의 left값과 top값을 커서의 XY 좌표값과 일치시킴
-function cursor(e) {
-  targetX = e.pageX;
-  targetY = e.pageY;
-}
+  // 커서의 left값과 top값을 커서의 XY 좌표값과 일치시킴
+  function cursor(e) {
+    targetX = e.pageX;
+    targetY = e.pageY;
+  }
 
-// 마우스 이동 시 cursor 함수 호출
-window.addEventListener("mousemove", cursor);
+  // 마우스 이동 시 cursor 함수 호출
+  window.addEventListener("mousemove", cursor);
 
+  // 커서에 애니메이션 적용
+  // https://developer.mozilla.org/ko/docs/Web/API/Window/rㅋequestAnimationFrame
+  function animateCursor() {
+    currentX += (targetX - currentX) * followSpeed;
+    currentY += (targetY - currentY) * followSpeed;
 
-// 커서에 애니메이션 적용
-// https://developer.mozilla.org/ko/docs/Web/API/Window/rㅋequestAnimationFrame
-function animateCursor() {
-  currentX += (targetX - currentX) * followSpeed;
-  currentY += (targetY - currentY) * followSpeed;
+    // 스크롤 위치를 포함하여 커서 위치를 설정
+    mouseCursor.style.left = currentX + "px";
+    mouseCursor.style.top = currentY + "px";
 
-  // 스크롤 위치를 포함하여 커서 위치를 설정
-  mouseCursor.style.left = currentX + "px";
-  mouseCursor.style.top = currentY + "px";
-  
-  // 반복
-  requestAnimationFrame(animateCursor);
-}
-animateCursor();
+    // 반복
+    requestAnimationFrame(animateCursor);
+  }
+  animateCursor();
 
-// 링크 및 버튼 호버 효과
-links.forEach(link => {
-  link.addEventListener("mouseenter", () => {
-    mouseCursor.classList.add("cursor-hover");
+  // 링크 및 버튼 호버 효과
+  links.forEach((link) => {
+    link.addEventListener("mouseenter", () => {
+      mouseCursor.classList.add("cursor-hover");
+    });
+    link.addEventListener("mouseleave", () => {
+      mouseCursor.classList.remove("cursor-hover");
+    });
   });
-  link.addEventListener("mouseleave", () => {
-    mouseCursor.classList.remove("cursor-hover");
+
+  // header
+  $(".btn-menu .open");
+
+  $(".btn-menu .open").click(function (e) {
+    $(".header").addClass("active");
+    $("body").addClass("hidden");
   });
-});
+
+  $(".btn-menu .close").click(function (e) {
+    $(".header").removeClass("active");
+    $("body").removeClass("hidden");
+  });
+
+  let headerOriginal = new SplitType(".header .sub-item .original", {
+    type: "chars",
+  });
+  let headerDummy = new SplitType(".header .sub-item .dummy", {
+    type: "chars",
+  });
+
+
+  $(".btn-menu").css({ opacity: 1, visibility: "visible" });
+  $(".nav .logo").css({ opacity: 1, visibility: "visible" });
+
+}); // document ready
